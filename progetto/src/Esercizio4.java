760c6d76551f09b6543e05d1e7ce49179f91e0be
SimoneS2000 <simones143@gmail.com>
2023-05-31 11:05:15 +0200

    aggiunta esercizio

--- progetto/src/Esercizio4.java
+++ progetto/src/Esercizio4.java
@@ -10,12 +10,16 @@ Scrivi un programma Java che chiede all'utente di inserire una stringa contenent
 public class Esercizio4 {
     public static void main(String[] args) {
         String frase="Nome: John, Cognome: Doe, Età: 25";
-        String regex="";
+        String regex="Nome: (.*), Cognome: (.*), Età: (.*)";
 
         Pattern pattern= Pattern.compile(regex);
         Matcher matcher= pattern.matcher(frase);
         while(matcher.find()){
-            System.out.println(matcher.group());
+            String nome=matcher.group(1);
+            String cognome=matcher.group(2);
+            String eta=matcher.group(3);
+            System.out.println(nome+" "+cognome+" "+eta);
+           // System.out.println(matcher.group());
         }
 
     }
---
+++ progetto/src/Esercizio5.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/*
+Convalida della password
+Scrivi un programma Java che chiede all'utente di inserire una password e verifica se la password inserita rispetta determinati requisiti
+ di complessità.
+Utilizza un'espressione regolare per convalidare la password, ad esempio, controllando la lunghezza minima,
+la presenza di lettere maiuscole, minuscole, numeri e caratteri speciali.
+Fai in modo che il programma stampi "Password valida" o "Password non valida" a seconda dell'esito della convalida.
+ */
+public class Esercizio5 {
+    public static void main(String[] args) {
+        Scanner mioScanner=new Scanner(System.in);
+
+        String password=mioScanner.nextLine();
+        String regex="^[a-zA-Z0-9$&!]{8}";
+
+        boolean isValid=password.matches(regex);
+        if(isValid){
+            System.out.println("La password e' valida");
+        }else {
+            System.out.println("La password non e' valida");
+        }
+    }
+
+}
