cc41409a5b0158a6e6b734d1855d5de719ac51d8
SimoneS2000 <simones143@gmail.com>
2023-06-04 14:06:11 +0200

    agg

--- progetto/src/Interface/Esercizio3.java
+++ progetto/src/Interface/Esercizio3.java
@@ -1,5 +1,6 @@
 package Interface;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
@@ -9,14 +10,18 @@ public class Esercizio3 {
     public static void main(String[] args) {
         //3.Implementa una Function che prende in input una lista di stringhe e restituisce una nuova lista contenente solo le stringhe che iniziano con la lettera "A".
         List<String> listString2= Arrays.asList("Andiamo", "ciao", "Domani", "Adesso","allora");
-        Consumer<String> consumer2=(strings)-> System.out.println(strings);
+        Consumer<List <String>> consumer2=(strings)-> {
+            for (String s : strings) {
+                System.out.println(s);
+            }
+        };
+        Function<String, List <String>> function=(strings)->{
 
-        Function<String, String> function=(strings)->{
-            String parola="";
-            if(strings.startsWith("A"))
-                parola=strings;
-            return parola;
-
+            List<String> listaParoleA=new ArrayList<>();
+            if(strings.startsWith("A")){
+                listaParoleA.add(strings);
+            }
+            return listaParoleA;
         };
 
         listString2.stream().map(function).forEach(consumer2);
